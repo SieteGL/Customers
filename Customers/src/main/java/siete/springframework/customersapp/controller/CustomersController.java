c1fe2385db5ee032c9ddff8d5aa7e98da0d0a420
SieteGL <77251952+SieteGL@users.noreply.github.com>
2021-12-22 11:45:57 -0300

    Log4j just added

--- Customers/src/main/java/siete/springframework/customersapp/controller/CustomersController.java
+++ Customers/src/main/java/siete/springframework/customersapp/controller/CustomersController.java
@@ -1,20 +1,27 @@
 package siete.springframework.customersapp.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import siete.springframework.customersapp.exception.CustomException;
 import siete.springframework.customersapp.exception.CustomerIdExistException;
 import siete.springframework.customersapp.exception.CustomerNotFoundException;
 import siete.springframework.customersapp.services.CustomersService;
 
 import siete.springframework.customersapp.model.Customers;
 
+//import org.apache.logging.log4j.LogManager;
+//import org.apache.logging.log4j.Logger;
+
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
 
 @RestController
 public class CustomersController {
@@ -23,6 +30,10 @@ public class CustomersController {
     CustomersService customersService;
 
     private static Map<String, Customers> customersRepo = new HashMap<>();
+    //private static final Logger LOG = LogManager.getLogger(CustomersController.class);
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CustomersController.class);
+
 
     /**static {
         Customers c1 = new Customers();
@@ -42,7 +53,9 @@ public class CustomersController {
 
     @RequestMapping(value = "/customers")
     public ResponseEntity<Object> getAllCustomers(){
-        if(customersService.getAllCustomers().size() == 0) throw new CustomerNotFoundException();
+        if(customersService.getAllCustomers().size() == 0) {
+            throw new CustomerNotFoundException();
+        }
         return new ResponseEntity<>(customersRepo.values(), HttpStatus.OK);
     }
 
@@ -63,7 +76,7 @@ public class CustomersController {
         return customers.getId();
     }**/
 
-    @RequestMapping(value = "/customers", method = RequestMethod.POST)
+    /**@RequestMapping(value = "/customers", method = RequestMethod.POST)
     public ResponseEntity<Object> createCustomer(@RequestBody Customers customers){
         System.out.println("****");
         System.out.println(customersRepo.containsKey(customers.getId()));
@@ -73,16 +86,40 @@ public class CustomersController {
         customersRepo.put(customers.getId(), customers);
         customersService.saveOrUpdate(customers);
         return new ResponseEntity<>("Customer is created Successfully", HttpStatus.CREATED);
+    }**/
+
+    @RequestMapping(value = "/customers", method = RequestMethod.POST)
+    public ResponseEntity<Object> createCustomer(@RequestBody Customers customers) {
+        if (customersRepo.containsKey(customers.getId())) {
+            //LOG.info("Error");
+            throw new CustomException(
+                    "Wrong ID",
+                    "You've entered an existing ID",
+                    "Check Again the Information",
+                    "Please change the ID",
+                    "Contact your Support");
+        }
+        customersRepo.put(customers.getId(), customers);
+        customersService.saveOrUpdate(customers);
+        return new ResponseEntity<>("Customer is created Successfully", HttpStatus.CREATED);
     }
 
-
     @RequestMapping(value = "/customers/{id}", method = RequestMethod.PUT)
-    public ResponseEntity<Object> updateCustomer(@PathVariable("id") String id, @RequestBody Customers customers){
+    public ResponseEntity<Object> updateCustomer(@PathVariable("id") String id,
+                                                 @RequestBody Customers customers){
 
-        System.out.println(id);
-        System.out.println(customersRepo.containsKey(id));
+        LOGGER.trace("doStuff needed more information - {}", customers);
+        LOGGER.debug("doStuff needed to debug - {}", id);
+        LOGGER.info("Error in this point - {}", customers.getAddress());
+        LOGGER.warn("doStuff needed to warn - {}", id);
+        LOGGER.error("doStuff encountered an error with value - {}", id);
 
-        if(!customersRepo.containsKey(id))throw new CustomerNotFoundException();
+        /**System.out.println(id);
+        System.out.println(customersRepo.containsKey(id));**/
+
+        if(!customersRepo.containsKey(id)) {
+            throw new CustomerNotFoundException();
+        }
         customersRepo.remove(id);
         customers.setId(id);
         customersRepo.put(id, customers);
@@ -90,15 +127,34 @@ public class CustomersController {
         return new ResponseEntity<>("Customer Is Updated Successfully", HttpStatus.OK);
     }
 
-    @RequestMapping(value = "/customers/{id}", method = RequestMethod.DELETE)
+    /**@RequestMapping(value = "/customers/{id}", method = RequestMethod.DELETE)
     public ResponseEntity<Object> delete(@PathVariable("id") String id){
-        System.out.println(id);
-        System.out.println(customersRepo.containsKey(id));
 
-        if(!customersRepo.containsKey(id))throw new CustomerNotFoundException();
+        if(!customersRepo.containsKey(id)) {
+            throw new CustomerNotFoundException();
+        }
         customersRepo.remove(id);
         customersService.delete(id);
         return new ResponseEntity<>("Customer is deleted Successfully", HttpStatus.OK);
+    }**/
+
+
+
+    @RequestMapping(value = "/customers/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<Object> delete(@PathVariable("id") String id){
+        try {
+            customersRepo.remove(id);
+            customersService.delete(id);
+        } catch (Exception e) {
+            LOGGER.info("Please verify the id that you want to delete", e);
+            throw new CustomException(
+                    "Please verify the ID",
+                    "Please be sure if the ID you want to delete exist",
+                    "Generate a consult to GET all the users",
+                    "Verify the data it's the same",
+                    "Contact your support if the issue persist");
+        }
+        return new ResponseEntity<>("Customer is deleted successfully", HttpStatus.OK);
     }
 
 
--- Customers/src/test/java/siete/springframework/customersapp/CustomersAppApplicationTests.java
+++ Customers/src/test/java/siete/springframework/customersapp/CustomersAppApplicationTests.java
@@ -1,13 +1,42 @@
 package siete.springframework.customersapp;
 
+import org.apache.coyote.Response;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
 
-@SpringBootTest
+import java.net.URI;
+import java.net.URISyntaxException;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class CustomersAppApplicationTests {
 
+    @LocalServerPort
+    private int randomServerPort;
+
     @Test
-    void contextLoads() {
+    public void testGetEmployeesListSuccess() throws URISyntaxException {
+        RestTemplate restTemplate = new RestTemplate();
+        final String baseurl = "http://localhost:" + randomServerPort + "/customers";
+        URI uri = new URI(baseurl);
+
+        ResponseEntity<String> result = restTemplate.getForEntity(uri, String.class);
+
+        //Verify request succeed
+        Assert.assertEquals(200, result.getStatusCodeValue());
+        Assert.assertEquals(true, result.getBody().contains("customers"));
+
     }
+
+
+
+
 
 }
